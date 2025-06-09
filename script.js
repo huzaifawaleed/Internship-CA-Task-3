12e783f9558ef924bb86b4401a541da0b58fe6cb
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-06-09 18:48:39 +0500

    Portfolio Update

--- script.js
+++ script.js
@@ -95,3 +95,17 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
 // Bottom to to button
+
+
+// const allPaths = document.querySelectorAll("path");
+// allPaths.forEach((path)=>{
+//     const length = path.getTotalLength();
+
+//     path.style.strokeDasharray = length;
+//     path.style.strokeDashoffset =length;
+
+//     path.getBoundingClientRect();
+
+//     path.style.transition = "stroke-dashoffset 3s ease-in-out";
+//     path.style.strokeDashoffset = "0";
+// });
